8740b50dc4174d6b497a836664a2b78f1e60a834
FrranzMaurer <38557647+FrranzMaurer@users.noreply.github.com>
2018-08-16 21:54:04 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 var prefix = "https://cors-anywhere.herokuapp.com/";
-var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
 function getQuote() {
     fetch(prefix + quoteUrl, { cache: "no-store" })
@@ -18,20 +17,20 @@ function createTweet(input) {
     dataElement.innerHTML = data.content;
     var quoteText = dataElement.innerText.trim();
     var quoteAuthor = data.title;
-
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
+    if (tweetText.length > 140) {
+    getQuote();
+	} else {
+	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    document.querySelector('.quote').innerText = quoteText;
+	    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+	    document.querySelector('.tweet').setAttribute('href', tweet);
+	}
 }
 
-if (tweetText.length > 140) {
-    getQuote();
-} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    document.querySelector('.quote').innerText = quoteText;
-    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-    document.querySelector('.tweet').setAttribute('href', tweet);
-}
 
 document.querySelector('.tweet').setAttribute('href', tweet);
 
